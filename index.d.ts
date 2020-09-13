36acfdff27060f5341edafbe3db41ac3408560c7
David Evans <davidje13@users.noreply.github.com>
2020-09-13 11:36:37 +0100

    Add Buffer and KeyObject as possible key types
    Fixes #97

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
+import { KeyLike } from 'crypto';
+
 export type TAlgorithm = 'HS256' | 'HS384' | 'HS512' | 'RS256';
 
 export interface IOptions {
   header: any;
 }
 
-export function decode(token: string, key: string, noVerify?: boolean, algorithm?: TAlgorithm): any;
+export function decode(token: string, key: string | Buffer, noVerify?: boolean, algorithm?: TAlgorithm): any;
+export function decode(token: string, key: KeyLike, noVerify: true): any;
+export function decode(token: string, key: KeyLike, noVerify: boolean, algorithm: TAlgorithm): any;
 
-export function encode(payload: any, key: string, algorithm?: TAlgorithm, options?: IOptions): string;
+export function encode(payload: any, key: KeyLike, algorithm?: TAlgorithm, options?: IOptions): string;
